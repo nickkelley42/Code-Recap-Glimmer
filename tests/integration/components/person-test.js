08b9c9ae87984e6ef2e1a5f15bd9a335a35cf06b
Nick Kelley <nkelley1@berklee.edu>
2021-02-19 09:25:10 -0800

    add tests n shit

--- tests/integration/components/person-test.js
+++ tests/integration/components/person-test.js
@@ -1,26 +1,51 @@
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import {
+  render,
+  click,
+} from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+import Service from '@ember/service';
+class MockSelectedPersonService extends Service {
+  constructor() {
+    super(...arguments);
+    this.selectPersonCalls = [];
+  }
+
+  selectPerson() {
+    for (let fn of this.selectPersonCalls) {
+      fn(...arguments);
+    }
+  }
+}
 
 module('Integration | Component | person', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function() {
+    this.owner.register('service:selected-person', MockSelectedPersonService);
+  })
 
-    await render(hbs`<Person />`);
+  test('it sends its person to SelectedPersonService', async function(assert) {
+    // Arrange
 
-    assert.equal(this.element.textContent.trim(), '');
+    this.person  = { name: 'Namey Nameson', planet: 'Mars' };
 
-    // Template block usage:
+    // Add an assertion to the mock service
+    const selectedService = this.owner.lookup('service:selected-person');
+    selectedService.selectPersonCalls.push((person) => {
+      assert.equal(person, this.person, "selectPerson takes a person as an argument");
+    });
+
+    // Act
     await render(hbs`
-      <Person>
-        template block text
-      </Person>
+      <Person @person={{person}} />
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await click('button[data-test-sel="edit-button');
+
+    // Assert
+    assert.expect(1);
   });
 });
